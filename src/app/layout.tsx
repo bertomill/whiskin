186cd9ceee7a3d4c496a62e42e93bc9f6ef7d149
Berto MIll <81169127+bertomill@users.noreply.github.com>
2025-07-31 07:08:45 -0400

    Create new whisk-in-motion icon for Whiskin app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
